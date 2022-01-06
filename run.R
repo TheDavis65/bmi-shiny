9376e5e9119631e964f3213b4df82ff6f2d6156b
Michael Davis <michaeldavis@mail.dk>
2022-01-06 17:01:55 +0100

    online commit

---
+++ run.R
@@ -0,0 +1,10 @@
+library(shiny)
+library(shinythemes)
+
+port <- Sys.getenv('PORT')
+
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
---
+++ server.R
@@ -0,0 +1,32 @@
+####################################
+# Server                           #
+####################################
+server <- function(input, output, session) {
+  
+  # Input Data
+  datasetInput <- reactive({  
+    
+    bmi <- input$weight/( (input$height/100) * (input$height/100) )
+    bmi <- data.frame(bmi)
+    names(bmi) <- "BMI"
+    print(bmi)
+    
+  })
+  
+  # Status/Output Text Box
+  output$contents <- renderPrint({
+    if (input$submitbutton>0) { 
+      isolate("Calculation complete.") 
+    } else {
+      return("Server is ready for calculation.")
+    }
+  })
+  
+  # Prediction results table
+  output$tabledata <- renderTable({
+    if (input$submitbutton>0) { 
+      isolate(datasetInput()) 
+    } 
+  })
+  
+}
